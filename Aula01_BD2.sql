d511a6d0e0e70dbebaaf83176bfa7b15fd937610
Eduardo Dourado <166930671+eduardoDC101@users.noreply.github.com>
2025-08-13 08:31:27 -0300

    Update Aula01_BD2.sql

--- Aula01_BD2.sql
+++ Aula01_BD2.sql
@@ -1,7 +1,7 @@
 -- Craição da tabela Aluno (O campo que será Primarykey, sempre será not null, automático)
 CREATE TABLE aluno(
 	prontuario VARCHAR (15),
-	nomeAluno VARCHAR (40) NOT NULL,
+	nome VARCHAR (40) NOT NULL,
 	idade INTEGER,
 	CONSTRAINT pk_aluno PRIMARY KEY (prontuario)
 );
@@ -12,10 +12,28 @@ DROP TABLE aluno;
 -- Inserir dados na tabela
 INSERT INTO aluno VALUES('VP3029342', 'Carlos Almeida', 20);
 -- ou inserir em colunas especificas
-INSERT INTO aluno (prontuario, nomeAluno)
-            VALUES ('VP3029481', 'João da Silva');
+INSERT INTO aluno (prontuario, nome)
+            VALUES ('VP3029441', 'João da Silva',21);
 -- ou
-INSERT INTO aluno (prontuario, idade, nomeAluno)
+INSERT INTO aluno (prontuario, idade, nome)
             VALUES ('VP3029481', 21, 'João da Silva'),
                    ('VP2032125', 19, 'José Pedro');
+
+-- Atualizar coluna com um novo valor (aceita condições do where)
+UPDATE aluno SET nome='Heitor Covalsck'
+		WHERE prontuario = 'VP3029441';
+-- ou
+UPDATE aluno SET nome='Heitor Covalsck'
+		WHERE prontuario = 'VP3029441' AND idade=21;
+-- ou
+UPDATE aluno SET idade=23
+		WHERE nome ='José Pedro' or nome='Carlos Almeida';
+
+--ou
+UPDATE aluno SET nome='Heitor Camorra', idade=51
+		WHERE prontuario = 'VP3029441';
+
+-- Deletar tabela inteira
+DELETE FROM aluno;
+
 
