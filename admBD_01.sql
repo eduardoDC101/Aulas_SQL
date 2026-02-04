c7ae5cafe7c453dfc197303c0b7d0d66a728fc3d
Eduardo Dourado <166930671+eduardoDC101@users.noreply.github.com>
2026-02-04 09:04:38 -0300

    Add emprestimo and empregado tables with data
    Created tables for 'emprestimo' and 'empregado' with constraints and sample data.

---
+++ admBD_01.sql
@@ -0,0 +1,34 @@
+CREATE TABLE emprestimo(
+	nome_ag_emp VARCHAR(15) NOT NULL,
+	numero_emp VARCHAR(10) NOT NULL,
+	valor_emp NUMERIC(10, 2),
+	CONSTRAINT pk_emprestimo PRIMARY KEY (numero_emp),
+	CONSTRAINT chk_valor_emprestimo CHECK (valor_emp > 100)
+);
+
+INSERT INTO emprestimo
+	VALUES ('Votuporanga', '11A', 150);
+
+SELECT * FROM emprestimo;
+
+--EX1:
+CREATE TABLE empregado(
+	idEmpregado INTEGER NOT NULL,
+	pNome VARCHAR(50) NOT NULL,
+	sNome VARCHAR(50) NOT NULL,
+	dtaNasc DATE,
+	dtaContr DATE,
+	salario REAL,
+	CONSTRAINT pk_empregado PRIMARY KEY (idEmpregado),
+	CONSTRAINT chk_salario_empregado CHECK (salario > 400),
+	CONSTRAINT chk_dtaNasc_empregado CHECK (dtaNasc > '01/01/2001'),
+	CONSTRAINT chk_dtaNasc_empregado CHECK (((dtaContr - dtaNasc)/365 >= 18))
+);
+
+INSERT INTO empregado
+	VALUES (111, 'Carlos', 'da Silva', '2005/03/03', '2026/04/02', 1518);
+
+SELECT * FROM empregado;
+
+
+
